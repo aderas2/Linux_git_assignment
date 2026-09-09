4097eef9c6a036982edd3496e8c8245f6de8ea15
aderas <bilaua1@gmail.com>
2026-09-09 17:46:38 +0100

    Updating the bash script codes for downloading the CSV files and storing it in a folder created

--- etl.sh
+++ etl.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/sh
 
 
+# Extracting the data from the URL
+
 echo "Extracting Data"
 
-export CSV_URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
-export Folder="./raw"
+# Defining the variables
+
+URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
+Folder="raw"
+File_Name="Enterprise_Survey"
+
+#creating the destination directory
+
+mkdir -p "$Folder"
+
+#Downloading the file using the URL
+
+curl -L "$URL" -o "$Folder/$File_name"
+
+echo "Download completed! Fild saved to "$Folder/$File_name"
+
+
+
 
 curl -l  CSV_URL >  Folder/raw.csv &  echo "Downloaded Successfully"
 
