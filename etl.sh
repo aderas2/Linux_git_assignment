bd4eaa97afa0a3e40ecbc57a36bf9067b2ac7238
aderas <bilaua1@gmail.com>
2026-09-09 17:32:54 +0100

    updating the bash scrip file

--- etl.sh
+++ etl.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
 
+
+echo "Extracting Data"
+
 export CSV_URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
 export Folder="./raw"
+
+curl -l  CSV_URL >  Folder/raw.csv &  echo "Downloaded Successfully"
+
+
